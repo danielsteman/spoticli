f008852f9ce91ee53a1b4bebb114d449c06271d3
Daniel Steman <daniel-steman@live.nl>
2022-11-16 19:35:09 +0100

    :construction: auth wip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 use reqwest;
+use base64;
+use std::collections::HashMap;
+
+async fn songs() {
+    let result = reqwest::get("https://api.spotify.com/v1/search").await;
+    println!("{:?}", result);
+}
+
+async fn auth() {
+    let client_id = "test";
+    let client_secret = "test";
+    let encoded = base64::encode(format!("{}:{}", client_id, client_secret));
+
+    let mut headers = HashMap::from([
+        ("url", "https://accounts.spotify.com/api/token"),
+        ("Authorization", &*format!("{}{}", "Basic ", encoded)),
+    ]);
+}
 
 #[tokio::main]
 async fn main() {
-    let result = reqwest::get("https://api.spotify.com/v1/search").await;
-    println!("{:?}", result);
+    auth().await
 }
